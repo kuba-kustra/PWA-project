d514345bf9d2748f84acbf52fc4c76495333f315
kuba-kustra <jakub.kkc@gmail.com>
2022-04-05 18:35:46 +0300

    v3

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
 window.onload = () => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js', {scope: '/index.html'});
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
   }
-}
