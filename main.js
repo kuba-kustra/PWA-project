de443be5c4734bf2d87a3edadd66358f067759ea
kuba-kustra <jakub.kkc@gmail.com>
2022-04-05 18:19:11 +0300

    test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
+window.onload = () => {
+  'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(r) {
-    console.log('ServiceWorker zarejestrowany.')
-    }).catch(function(e) {
-    console.log('Ups! Błąd przy rejestracji ServiceWorkera! '+e)
-    });
-    }
+    navigator.serviceWorker
+             .register('./sw.js', {scope: '/index.html'});
+  }
+}
