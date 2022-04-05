6909833f0aaccc9f17a7d175c768144ed178d53a
kuba-kustra <jakub.kkc@gmail.com>
2022-04-05 18:59:33 +0300

    test

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ var cacheName = 'PWA Project';
 var filesToCache = [
   './',
   './index.html',
-  './js/main.js'
+  './main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
